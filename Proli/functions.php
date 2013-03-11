fc6a5f010ed0f9fc2925b9b19d981ac53b6f3312
Soon Hong <xoulrage@gmail.com>
2013-03-11 21:59:28 +0800

    vinoth's changes

--- Proli/functions.php
+++ Proli/functions.php
@@ -6,10 +6,12 @@ if ( function_exists( 'add_theme_support' ) ) {
     add_image_size( 'homepage-thumb', 180, 215, true );
     add_image_size( 'construction-thumb', 177, 126, true );
     add_image_size( 'development-thumb', 166, 137, true );
-    add_image_size( 'slider_thumb', 164, 262 );
+    add_image_size( 'slider_thumb', 260, 120, true );
+	
     function custom_excerpt_length( $length ) {
-        return 50;
+        return 20;
     }
+	
     add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
     add_filter('single_template', create_function('$t', 'foreach( (array) get_the_category() as $cat ) { if ( file_exists(TEMPLATEPATH . "/single-{$cat->slug}.php") ) return TEMPLATEPATH . "/single-{$cat->slug}.php"; } return $t;' ));
